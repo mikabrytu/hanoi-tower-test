adf93df357e4eab2a99181b54c7790cb389d6b8f
Mikabrytu <mikabrytu@gmail.com>
2021-04-13 10:27:03 -0300

    Freeze X position when ring is stuck on pin

--- Hanoi Tower Test/Assets/Scripts/Components/RingComponent.cs
+++ Hanoi Tower Test/Assets/Scripts/Components/RingComponent.cs
@@ -8,19 +8,53 @@ namespace Mikabrytu.HanoiTower.Components
 {
     public class RingComponent : MonoBehaviour
     {
+        [Header("Pin Properties")]
+        [SerializeField] private string _pinTag;
+
         private InputSystem inputSystem;
         private MoveSystem moveSystem;
+        private Rigidbody2D rigidbody;
+
+        private Vector3 pinPosition;
+        private bool stuckOnPin = false;
 
         private void Start()
         {
             inputSystem = new InputSystem(transform, Camera.main);
             moveSystem = new MoveSystem(transform, Camera.main);
+
+            rigidbody = GetComponent<Rigidbody2D>();
         }
 
         private void Update()
         {
             if (inputSystem.IsTouching())
-                moveSystem.Move(inputSystem.GetTouchPosition());
+            {
+                rigidbody.isKinematic = true;
+
+                if (stuckOnPin)
+                    moveSystem.Move(inputSystem.GetTouchPosition(), Vector3.right, pinPosition);
+                else
+                    moveSystem.Move(inputSystem.GetTouchPosition());
+            } else
+            {
+                rigidbody.isKinematic = false;
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (collision.tag.Equals(_pinTag))
+            {
+                stuckOnPin = true;
+                pinPosition = collision.transform.position;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (collision.tag.Equals(_pinTag))
+                stuckOnPin = false;
         }
     }
 }
--- Hanoi Tower Test/Assets/Scripts/Systems/MoveSystem.cs
+++ Hanoi Tower Test/Assets/Scripts/Systems/MoveSystem.cs
@@ -13,12 +13,34 @@ namespace Mikabrytu.HanoiTower.Systems
             this.camera = camera;
         }
 
+        public void Move(Vector3 position, Vector3 fixedAxis, Vector3 fixedPosition)
+        {
+            position = CastScreenToWorld(position);
+
+            if (fixedAxis.x > 0)
+                position.x = fixedPosition.x;
+
+            if (fixedAxis.y > 0)
+                position.y = fixedPosition.y;
+
+            if (fixedAxis.z > 0)
+                position.z = fixedPosition.z;
+
+            transform.position = position;
+        }
+
         public void Move(Vector3 position)
+        {
+            position = CastScreenToWorld(position);
+            transform.position = position;
+        }
+
+        private Vector3 CastScreenToWorld(Vector3 position)
         {
             position = camera.ScreenToWorldPoint(position);
             position.z = 0;
 
-            transform.position = position;
+            return position;
         }
     }
 }
